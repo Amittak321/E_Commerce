6a5d0c707673b24435c122617d94d1e41bbea5e6
amittak321 <amittak321@gmail.com>
2023-04-06 12:30:20 +0530

    user schema bug fix

--- models/user.schema.js
+++ models/user.schema.js
@@ -37,7 +37,7 @@ const userSchema = mongoose.Schema(
 
 // Challenge -1 encrpt the password 
 userSchema.pre("save", async function(next){
-    if(!this.modified("password")) return next();
+    if(!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
