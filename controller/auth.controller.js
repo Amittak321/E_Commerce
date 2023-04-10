2b240d9812608291386e7e93a504262431633b6b
amittak321 <amittak321@gmail.com>
2023-04-10 17:28:34 +0530

    authController -forgotPaddword

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.schema";
 import asyncHandler from "../services/asyncHandler";
 import CustomError from "../utils/customError";
+import mailHelper from "../utils/mailHelper";
 
 export const cookieOptions = {
   expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
@@ -88,15 +89,13 @@ export const login = asyncHandler(async (req, res) => {
 
   const token = user.getJwtToken();
   user.password = undefined;
-  res.cookie("token",token, cookieOptions);
+  res.cookie("token", token, cookieOptions);
   res.status(200).json({
     success: true,
     token,
-    user
+    user,
   });
-
 });
-
 
 /************************************************************
 
@@ -108,15 +107,72 @@ export const login = asyncHandler(async (req, res) => {
 
 *************************************************************/
 
-export const logout = asyncHandler(async(_req,res)=>{
-    // res.clearCookie()
-    res.cookie("token" ,null, {
-        expires: new Date(Date.now()),
-        httpOnly : true
+export const logout = asyncHandler(async (_req, res) => {
+  // res.clearCookie()
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "logout",
+  });
+});
+
+/************************************************************
+
+@FORGOT_PASSWORD
+@route http://localhost:4000/api/auth/password/forgot
+@description User will submit email and we will generate a token
+@parameters email
+@return success message - email send 
+
+*************************************************************/
+
+export const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    throw new CustomError("Please enter a email", 400);
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError("Invalid user", 404);
+  }
+
+  const resetToken = user.generateForgotPasswordToken();
+
+  // await user.save() ; // it will create a problem, required all the feilds
+  //solution
+  await user.save({ validateBeforeSave: false });
+
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/password/reset/${resetToken}`;
+
+  const text = `Your passeord reset url us 
+  \n\n ${resetUrl}\n\n`;
+
+  try {
+    await mailHelper({
+      email: user.email,
+      subject: "Password Reset email",
+      text: text,
     });
 
     res.status(200).json({
-        success : true,
-        message : 'logout'
-    })
-})
+      success: true,
+      message: `Email send to ${user.email}`,
+    });
+  } catch (err) {
+    //rollback - clear fields and save
+    user.forgotPasswordToken = undefined;
+    user.forgotPasswordExpiry = undefined;
+    user.save({validateBeforeSave:false});
+
+    throw new CustomError(err.message || 'Email sent failure', 500);
+  }
+});
